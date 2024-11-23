e0b0ec4be22c180d578276212be5fa721dc8df9c
Jihyeok <qowlgur121@gmail.com>
2024-11-23 23:16:04 +0900

    Initial commit

---
+++ src/main/java/com/jihyeok/springbootweb/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.jihyeok.springbootweb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
+
---
+++ src/main/java/com/jihyeok/springbootweb/SpringBootWebApplication.java
@@ -0,0 +1,12 @@
+package com.jihyeok.springbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+}
+
